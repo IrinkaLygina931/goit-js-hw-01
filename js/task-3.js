23f63f4eccf379e0c0dae4d30b4b9bfb0add06d0
IrinkaLygina931 <114241757+IrinkaLygina931@users.noreply.github.com>
2024-01-31 11:40:22 +0200

    add

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,7 @@
 
   function getElementWidth(content, padding, border) {
-  const contentWidth = parseFloat(content);
-  const paddingWidth = parseFloat(padding);
+  const contentWidth = parseFloat(content) * 2;
+  const paddingWidth = parseFloat(padding) * 2;
   const borderWidth = parseFloat(border);
   
   return contentWidth + paddingWidth + borderWidth;
